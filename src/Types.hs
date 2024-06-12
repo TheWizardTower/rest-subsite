7c94ee5709695463e2c034fe9e8d20be762c479b
Adam McCullough <merlinfmct87@gmail.com>
2024-06-12 12:12:39 -0700

    Migrate to manual To/FromJSON instances.
    We have to do a lot of unpleasant ceremony to make each field name
    unique so the accessors aren't ambiguous, but we should be gentleman and
    not insist that our end-users are thusly restricted.

--- src/Types.hs
+++ src/Types.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE DeriveAnyClass #-}
 {-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE OverloadedStrings #-}
 
 {- |
 Module: Types and derivations for the example REST endpoint
@@ -39,7 +40,13 @@ data CreateTodoRequest = CreateTodoRequest
     , ctrDescription :: Text
     -- ^ More in-depth description of the work to be done.
     }
-    deriving (Eq, Ord, Show, Generic, ToJSON, FromJSON)
+    deriving (Eq, Ord, Show, Generic)
+
+instance FromJSON CreateTodoRequest where
+    parseJSON (Object v) = CreateTodoRequest <$> v .: "title" <*> v .: "description"
+
+instance ToJSON CreateTodoRequest where
+    toJSON (CreateTodoRequest ctrTitle ctrDescription) = object ["title" .= ctrTitle, "description" .= ctrDescription]
 
 {- | Internal representation of the response of the create todo
  request. If successful, this will return the todo ID for the user to
@@ -49,7 +56,13 @@ data CreateTodoResponse = CreateTodoResponse
     { ctrTid :: Int
     -- ^ ID of the todo that was created.
     }
-    deriving (Eq, Ord, Show, Generic, ToJSON, FromJSON)
+    deriving (Eq, Ord, Show)
+
+instance FromJSON CreateTodoResponse where
+    parseJSON (Object v) = CreateTodoResponse <$> v .: "tid"
+
+instance ToJSON CreateTodoResponse where
+    toJSON (CreateTodoResponse ctrTid) = object ["tid" .= ctrTid]
 
 {- | If the create request fails, return the request that failed. Don't
  expose the internal error, as that's a security vulnerability. But, we
@@ -60,7 +73,13 @@ data FailedToCreateTodo = FailedToCreateTodo
     { createReq :: CreateTodoRequest
     -- ^ The originating create request type.
     }
-    deriving (Eq, Ord, Show, Generic, ToJSON, FromJSON)
+    deriving (Eq, Ord, Show)
+
+instance FromJSON FailedToCreateTodo where
+    parseJSON (Object v) = FailedToCreateTodo <$> v .: "createReq"
+
+instance ToJSON FailedToCreateTodo where
+    toJSON (FailedToCreateTodo createReq) = object ["createReq" .= createReq]
 
 {- | This struct is used to represent the result of a read request. As
 such, it has the title, description, and completed fields you'd
@@ -73,21 +92,35 @@ data TodosStruct = TodosStruct
     -- ^ More in-depth description of the work to be done.
     , todoCompleted :: Bool
     -- ^ Has the task been completed? True for yes.
-    , tsTid :: Int
+    , todoId :: Int
     -- ^ What ID did the database assign to this task?
     }
-    deriving (Eq, Ord, Show, Generic, ToJSON, FromJSON, FromRow, ToRow, FromField)
+    deriving (Eq, Ord, Show, Generic, FromRow, ToRow, FromField)
+
+instance FromJSON TodosStruct where
+    parseJSON (Object v) = TodosStruct <$> v .: "title" <*> v .: "description" <*> v .: "completed" <*> v .: "tid"
+
+instance ToJSON TodosStruct where
+    toJSON (TodosStruct tTitle tDescription tCompleted tId) = object ["title" .= tTitle, "description" .= tDescription, "completed" .= tCompleted, "tid" .= tId]
+
+-- instance FromField for TodosStruct where
+--   fromField =
+--     TodosStruct <$> field <*> field <*> field <*> field
 
 {- | This struct is returned as a JSON object if we try to read,
 update, or delete a todo ID that cannot be found.
 -}
 data TodoNotFound = TodoNotFound
     { tnfTid :: Int
-    -- ^ The missing Todo ID
+    -- ^ The TID that we could not find in our data store.
     }
-    -- \^ The TID that we could not find in our data store.
+    deriving (Eq, Ord, Show)
 
-    deriving (Eq, Ord, Show, Generic, ToJSON, FromJSON)
+instance FromJSON TodoNotFound where
+    parseJSON (Object v) = TodoNotFound <$> v .: "tid"
+
+instance ToJSON TodoNotFound where
+    toJSON (TodoNotFound tid) = object ["tid" .= tid]
 
 {- | We return this type if we run afoul of the invariant that each
 todo has a unique ID. This really only exists to handle a circumstance
@@ -97,7 +130,13 @@ data MultipleTodosFound = MultipleTodosFound
     { mtfTid :: Int
     -- ^ The TID that somehow was found more than once.
     }
-    deriving (Eq, Ord, Show, Generic, ToJSON, FromJSON)
+    deriving (Eq, Ord, Show)
+
+instance FromJSON MultipleTodosFound where
+    parseJSON (Object v) = MultipleTodosFound <$> v .: "tid"
+
+instance ToJSON MultipleTodosFound where
+    toJSON (MultipleTodosFound mTid) = object ["tid" .= mTid]
 
 {- | An update request. If a field is Nothing, then it will be
 unchanged in the request. Note that this allows for a "no-op" update,
@@ -112,7 +151,13 @@ data UpdateTodoRequest = UpdateTodoRequest
     , utrFinished :: Maybe Bool
     -- ^ A possible change to the finished state.
     }
-    deriving (Eq, Ord, Show, Generic, ToJSON, FromJSON)
+    deriving (Eq, Ord, Show)
+
+instance FromJSON UpdateTodoRequest where
+    parseJSON (Object v) = UpdateTodoRequest <$> v .:? "title" <*> v .:? "description" <*> v .:? "done"
+
+instance ToJSON UpdateTodoRequest where
+    toJSON (UpdateTodoRequest uTitle uDescription uFinished) = object ["title" .= uTitle, "description" .= uDescription, "done" .= uFinished]
 
 {- | The response returned -- as a JSON object -- on successfully
 updating a todo entry.
@@ -121,7 +166,13 @@ data UpdateTodoResponse = UpdateTodoResponse
     { utrTid :: Int
     -- ^ Update was successful, return TID update was performed on
     }
-    deriving (Eq, Ord, Show, Generic, ToJSON, FromJSON)
+    deriving (Eq, Ord, Show)
+
+instance FromJSON UpdateTodoResponse where
+    parseJSON (Object v) = UpdateTodoResponse <$> v .: "tid"
+
+instance ToJSON UpdateTodoResponse where
+    toJSON (UpdateTodoResponse uTid) = object ["tid" .= uTid]
 
 {- | The response returned -- as a JSON object -- on successfully
 deleting a todo entry. Hopefully one we've finished, rather than
@@ -131,4 +182,10 @@ data DeleteTodoResponse = DeleteTodoResponse
     { dtrTid :: Int
     -- ^ Update was successful, return TID update was performed on
     }
-    deriving (Eq, Ord, Show, Generic, ToJSON, FromJSON)
+    deriving (Eq, Ord, Show)
+
+instance FromJSON DeleteTodoResponse where
+    parseJSON (Object v) = DeleteTodoResponse <$> v .: "tid"
+
+instance ToJSON DeleteTodoResponse where
+    toJSON (DeleteTodoResponse dTid) = object ["tid" .= dTid]
